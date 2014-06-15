5965e11549cabf826c83077853948a52a7af510c
ryosuke1015 <yasi@takashimaryousuke-no-Mac.local>
2014-06-15 08:05:12 -0700

    061601
    ファーストコミット

---
+++ calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  calculator
+//
+//  Created by 高島良祐 on 2014/06/15.
+//  Copyright (c) 2014年 Ryosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
